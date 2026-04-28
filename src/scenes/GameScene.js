e8bfa5d6743fedfa2d995fea48258dcb58d61145
nemocua-ebc <nemocua-ebc@users.noreply.github.com>
2026-04-28 17:38:15 +0000

    fix: use namespace import for Phaser ESM (no default export)

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser'
+import * as Phaser from 'phaser'
 import Player from '../entities/Player.js'
 import Enemy from '../entities/Enemy.js'
 import NPC from '../entities/NPC.js'
